f93619ccbffff343733afeb344e55749cacb09ec
Gerson Victor <gerson.vfs@gmail.com>
2022-07-28 23:34:47 -0300

    add XMLData class

--- index.dart
+++ index.dart
@@ -1,6 +1,7 @@
 import 'lib/json_data.dart';
+import 'lib/xml_data.dart';
 
-void main(){
+void main() {
   var jsonFileName = './example_files/json_example.json';
   var jSONData = JSONData();
   jSONData.load(jsonFileName);
@@ -9,4 +10,13 @@ void main(){
   jSONData.save('./example_files/json_example_out.json');
   jSONData.clear();
   print(jSONData.hasData);
+
+  var xmlFileName = './example_files/xml_example.xml';
+  var xmlData = XMLData();
+  xmlData.load(xmlFileName);
+  print(xmlData.hasData);
+  print(xmlData.fields);
+  // xmlData.save('./example_files/xml_example_out.json');
+  // xmlData.clear();
+  // print(xmlData.hasData);
 }
---
+++ lib/xml_data.dart
@@ -0,0 +1,74 @@
+import 'dart:io';
+import 'package:xml/xml.dart';
+import 'data.dart';
+
+class XMLData implements Data {
+  String? _rawXML = null;
+  List<Map<String, dynamic>>? _mapXML = null;
+
+  String? get data => _rawXML;
+
+  void set data(String? raw) {
+    if (raw == null) {
+      throw Exception("Invalid XML data");
+    }
+
+    final document = XmlDocument.parse(raw);
+
+    final root = document.getElement("root");
+    if (root == null) {
+      throw Exception("Invalid XML data");
+    }
+
+    final elements = root.findElements("element");
+    if (elements.isEmpty) {
+      throw Exception("Invalid XML data");
+    }
+
+    final List<Map<String, dynamic>> result = [];
+    for (var el in elements) {
+      Map<String, dynamic> data = {};
+      for (var el in el.childElements) {
+        data[el.name.toString()] = el.text;
+      }
+      result.add(data);
+    }
+
+    _mapXML = result;
+    _rawXML = raw;
+  }
+
+  bool get hasData => data != null;
+
+  void clear() {
+    data = null;
+  }
+
+  List<String> get fields {
+    Set<String> keys = {};
+
+    if (_mapXML == null) {
+      return keys.toList();
+    }
+
+    for (var el in _mapXML!) {
+      for (var key in el.keys) {
+        keys.add(key);
+      }
+    }
+
+    return keys.toList();
+  }
+
+  void load(String fileName) {
+    final file = new File(fileName);
+    try {
+      final raw = file.readAsStringSync();
+      data = raw;
+    } catch (e) {
+      throw e; // TODO
+    }
+  }
+
+  void save(String fileName) {}
+}
