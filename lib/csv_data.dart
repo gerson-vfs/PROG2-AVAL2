3101f2a01bd739724b140a4d3211cf9a171a4ee0
João Neto Castro <jneto111@gmail.com>
2022-07-30 10:29:26 -0300

    refactor: move DelimitedData subclasses implementation to abstract class DelimitedData

--- lib/csv_data.dart
+++ lib/csv_data.dart
@@ -2,39 +2,6 @@ import 'dart:io';
 import 'package:csv/csv.dart';
 import 'delimited_data.dart';
 
-class CSVData implements DelimitedData {
-  List<List<dynamic>> rows = [];
-  String? data='';
-
-  bool hasData=false;
-
-  List<String> get fields => rows.length > 0 ? rows[0].map((field) => field.toString()).toList() : [];
-
-  void load(String fileName) {
-    final csvString = File(fileName).readAsStringSync();
-    // TODO: throw FileNotFoundError se não encontrar
-    data = csvString;
-    if(data != ''){
-      // TODO: throw InvalidFile se não conseguir parsear o CSV
-      rows = const CsvToListConverter().convert(csvString, eol:"\n");
-      hasData = true;
-    }else {
-      rows = [];
-      hasData = false;
-    }
-  }
-
-  void save(String fileName) {
-    final outFile = File(fileName);
-    outFile.createSync(recursive: true);
-    outFile.writeAsStringSync(data ?? '');
-  }
-
-  void clear() {
-    rows = [];
-    data = '';
-    hasData = false;
-  }
-
+class CSVData extends DelimitedData {
   String get separator => ',';
 }
--- lib/delimited_data.dart
+++ lib/delimited_data.dart
@@ -1,6 +1,40 @@
 import './data.dart';
+import 'dart:io';
+import 'package:csv/csv.dart';
 
 abstract class DelimitedData extends Data {
+  List<List<dynamic>> rows = [];
+  String? data='';
+
+  bool hasData=false;
+
+  List<String> get fields => rows.length > 0 ? rows[0].map((field) => field.toString()).toList() : [];
+
+  void load(String fileName) {
+    final tsvString = File(fileName).readAsStringSync();
+    // TODO: throw FileNotFoundError se não encontrar
+    data = tsvString;
+    if(data != ''){
+      // TODO: throw InvalidFile se não conseguir parsear o TSV
+      rows = const CsvToListConverter().convert(tsvString, eol: '\n', fieldDelimiter: separator);
+      hasData = true;
+    }else {
+      rows = [];
+      hasData = false;
+    }
+  }
+
+  void save(String fileName) {
+    final outFile = File(fileName);
+    outFile.createSync(recursive: true);
+    outFile.writeAsStringSync(data ?? '');
+  }
+
+  void clear() {
+    rows = [];
+    data = '';
+    hasData = false;
+  }
+
   String get separator;
 }
-
